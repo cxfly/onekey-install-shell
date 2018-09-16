d5a3ed1df55d3708c871529217d96fc9b8767172
cxfly <htzd007@gmail.com>
2018-09-16 09:02:19 +0800

    Update version.sh

--- frps/version.sh
+++ frps/version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export FRPS_VER=0.21.0
 export FRPS_INIT="hhttps://raw.githubusercontent.com/cxfly/onekey-install-shell/master/frps/version.sh"
-export aliyun_download_url="https://code.aliyun.com/clangcn/frp/raw/master"
+#export aliyun_download_url="https://code.aliyun.com/clangcn/frp/raw/master"
 export github_download_url="https://github.com/fatedier/frp/releases/download"
